456d0e9c29081a27ea9e24e586be48e8dc01e07f
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-03-22 15:10:49 +0900

    fix(package): drop duplicate String data initializer

--- Sources/MachOExtensions/String+.swift
+++ Sources/MachOExtensions/String+.swift
@@ -25,21 +25,6 @@ extension String {
 }
 
 extension String {
-    package init?(cString data: Data) {
-        guard !data.isEmpty else { return nil }
-        let string: String? = data.withUnsafeBytes {
-            guard let baseAddress = $0.baseAddress else { return nil }
-            let ptr = baseAddress.assumingMemoryBound(to: CChar.self)
-            return String(cString: ptr)
-        }
-        guard let string else {
-            return nil
-        }
-        self = string
-    }
-}
-
-extension String {
     @inline(__always)
     package func isEqual(to tuple: CCharTuple16) -> Bool {
         withUnsafePointer(to: tuple) { tuple in
